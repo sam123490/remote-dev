79a7bc7bd416702f1baa633dbb88753335592d46
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-06-21 13:16:26 -0700

    refactoring code

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,14 @@
 import {
     searchInputEl,
     searchFormEl,
-    spinnerSearchEl,
     jobListSearchEl,
-    numberEl
-} from '../common.js'
+    numberEl,
+    BASE_API_URL
+} from '../common.js';
+import renderError from './Error.js';
+import renderSpinner from './Spinner.js';
+import renderJobList from './JobList.js';
+
 
 const submitHandler = event => {
     //prevent default behavior
@@ -17,11 +21,8 @@ const submitHandler = event => {
     const forbiddenPattern = /[0-9]/;
     const patternMatch = forbiddenPattern.test(searchText);
     if (patternMatch) {
-        errorTextEl.textContent = 'Your search may not contain numbers';
-        errorEl.classList.add('error--visible');
-        setTimeout(() => {
-            errorEl.classList.remove('error--visible');
-        }, 4000);
+        renderError('Your search may not contain numbers');
+        return;
     }
 
     // blur input
@@ -31,10 +32,10 @@ const submitHandler = event => {
     jobListSearchEl.innerHTML = '';
 
     //show spinner
-    spinnerSearchEl.classList.add('spinner--visible');
+    renderSpinner('search');
 
     //fetch search results
-    fetch(`https://bytegrad.com/course-assets/js/2/api/jobs?search=${searchText}`)
+    fetch(`${BASE_API_URL}/jobs?search=${searchText}`)
         .then(response => {
             if (!response.ok) {
                 console.log('Something went wrong!');
@@ -48,35 +49,13 @@ const submitHandler = event => {
             const { jobItems } = data;
 
             //remove spinner
-            spinnerSearchEl.classList.remove('spinner--visible');
+            renderSpinner('search');
 
             //render number of results
             numberEl.textContent = jobItems.length;
 
             //render job items
-            jobItems.slice(0, 7).forEach(jobItem => {
-                const newJobItemHTML = `
-                    <li class="job-item">
-                        <a class="job-item__link" href="${jobItem.id}">
-                            <div class="job-item__badge">${jobItem.badgeLetters}</div>
-                            <div class="job-item__middle">
-                                <h3 class="third-heading">${jobItem.title}</h3>
-                                <p class="job-item__company">${jobItem.company}</p>
-                                <div class="job-item__extras">
-                                    <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${jobItem.salary}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobItem.location}</p>
-                                </div>
-                            </div>
-                            <div class="job-item__right">
-                                <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                                <time class="job-item__time">${jobItem.daysAgo}d</time>
-                            </div>
-                        </a>
-                    </li>
-                `;
-                jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
-            });
+            renderJobList(jobItems);
         })
         .catch(error => {
             console.log(`There was an error. Error message: ${error.message}`);
